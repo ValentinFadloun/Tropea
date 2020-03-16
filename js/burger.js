2a6fda7c4948663b3e569d94a49ae228fad9d5c3
Clara GIAQUINTO <56297757+Clara-g@users.noreply.github.com>
2020-03-16 15:35:22 +0100

    Header responsive all mobiles : version 1

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,5 @@
-//Menu burger//
 
-const NAV_CONTENT = document.querySelector('#nav_content');
-// const SECTION_BURGER = document.querySelector('#section_burger');
-// SECTION_BURGER.innerHTML=NAV_CONTENT;
-const BTN_BURGER = document.getElementsByClassName('burger')[0]; // [button_burger_1]
- BTN_BURGER.addEventListener('click', function () {
-    NAV_CONTENT.classList.toggle('nav-open');
-    BTN_BURGER.classList.toggle('btn-open');
- });
+
+
+ 
  
